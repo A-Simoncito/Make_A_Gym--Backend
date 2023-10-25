4818975dd8b831f866edf149723e4dc592703768
Escudero Ibañez Angel Simón <as.escudero@etec.local>
2023-10-25 13:00:38 -0300

    punto3

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,47 @@
 import {Router} from "express";
+// import express from "express";
+// const app = express();
+
 const router: Router = Router();       // instancia 
 router.get ("/", (_, res) => {   //ruta,res. sol. raiz, 
-    res.send ("Se mudo, funca");  //parametro
+    res.send ("El servidor anda en / ");  //parametro
 });
 
 export default router
+
+
+const  ProductoYmercancia = [
+    {
+      nombre: "Bicicleta",
+      modelo: "Modelo A",
+      precio: 900,
+      paisOrigen: "Hungria",
+    },
+    {
+      nombre: "Teclado ReDragon",
+      modelo: "Modelo RGB",
+      precio: 90,
+      paisOrigen: "EE.UU",
+    },
+    {
+      nombre: "Nissan GTR",
+      modelo: "Nismo",
+      precio: 9800,
+      paisOrigen: "Japon",
+    },
+];
+
+
+//punto 1
+router.get('/punto1', (_, res) => {
+    res.send(ProductoYmercancia);
+});
+        
+//punto 2
+router.get('/punto2', (_, res) => {
+    const punto2 = ProductoYmercancia.filter(producto => producto.precio > 100);
+    res.send(punto2);
+});
+
+//punto 3
+
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,6 @@ import router from "./router/routes"
 const app = express ();
 const port = 8080
 
-// app.get ("/", (_,res) => { //manda la respuesta 
-//     res.send("STEAM bajo los impuestos");
-// });
 
 app.use ("/", router)
 
