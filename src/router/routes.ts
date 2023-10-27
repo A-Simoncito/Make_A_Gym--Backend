c44f156c9c3f643e98993742d57f9a19b9176b16
Escudero Ibañez Angel Simón <as.escudero@etec.local>
2023-10-27 12:18:42 -0300

    7Puntos

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
+import express, {Request, Response} from "express";
+
 // import express from "express";
 // const app = express();
 
-const router: Router = Router();       // instancia 
+const router: Router = express.Router();       // instancia 
 router.get ("/", (_, res) => {   //ruta,res. sol. raiz, 
     res.send ("El servidor anda en / ");  //parametro
 });
@@ -10,9 +12,9 @@ router.get ("/", (_, res) => {   //ruta,res. sol. raiz,
 export default router
 
 
-const  ProductoYmercancia = [
+let  ProductoYmercancia = [
     {
-      nombre: "Bicicleta",
+      nombre: "Cuatri 4X4",
       modelo: "Modelo A",
       precio: 900,
       paisOrigen: "Hungria",
@@ -44,4 +46,46 @@ router.get('/punto2', (_, res) => {
 });
 
 //punto 3
+
+//punto 4
+router.get('/punto4/:modelo', (req, res) => {
+  const { modelo } = req.params;
+  const longitud = ProductoYmercancia.length;
+
+  ProductoYmercancia = ProductoYmercancia.filter(producto => producto.modelo !== modelo);
+  console.log("Funca");
+  
+  if (ProductoYmercancia.length === longitud) {
+    res.status(404).send("No se encontró el producto");
+  } else {
+    res.send("Se eliminó el producto");
+  }
+});
+    
+
+//Punto 5
+router.get ('/punto5/:pais' ,(req: Request, res: Response) => {
+  const { pais } =req.params;
+  const encontrarProdcuto = ProductoYmercancia.find(producto => producto.paisOrigen ===pais);
+
+  if (encontrarProdcuto) {
+    res.send(encontrarProdcuto);
+  } else {
+    res.status(404).send ("No se encontro el producto");
+  }
+});
+
+//Punto 6
+router.get('/punto6/:precio', (req: Request, res: Response) => {
+  const { precio } = req.params;
+  const buscarPrecio = ProductoYmercancia.filter(producto => producto.precio === parseInt(precio));
+
+  if (buscarPrecio.length > 0) {
+    res.send(buscarPrecio);
+  } else {
+    res.status(404).send("No se encontró el precio");
+  }
+});
+
+//punto 7
 
